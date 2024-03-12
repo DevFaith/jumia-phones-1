45e6312f39edbe68f1f0b6c70ea9072325e41123
DevFaith <waitherafaith@outlook.com>
2024-03-12 07:48:08 +0300

    * chore(main.js): update image paths for Phone1, Phone2, Phone3, and Phone4 objects

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,5 @@
 var Phone1 = {
-    Image: "/images/pexels-mohi-syed-454699.jpg",
+    Image: "/images/phone4.jpg",
     iphone: "image",
     name: "iphone SE",
     discountedPrice: "Ksh" + " " + 30000,
@@ -12,7 +12,7 @@ var Phone1 = {
   document.getElementById("originalPrice1").innerText = Phone1.originalPrice;
   
   var Phone2 = {
-    Image: "/images/pexels-mohi-syed-47261.jpg",
+    Image: "/images/phone3.jpg",
     iphone: "image2",
     name: "Iphone 10",
     discountedPrice: "Ksh" + " " + 55000,
@@ -25,7 +25,7 @@ var Phone1 = {
   document.getElementById("originalPrice2").innerText = Phone2.originalPrice;
   
   var Phone3 = {
-    Image: "/images/pexels-mateusz-dach-1294886.jpg",
+    Image: "/images/phone2.jpg",
     iphone: "image3",
     name: "Iphone 11",
     discountedPrice: "Ksh" + " " + 60000,
@@ -38,7 +38,7 @@ var Phone1 = {
   document.getElementById("originalPrice3").innerText = Phone3.originalPrice;
   
   var Phone4 = {
-    Image: "/images/pexels-essow-k-1042143.jpg",
+    Image: "/images/phone1.jpg",
     iphone: "image4",
     name: "Iphone 12",
     discountedPrice: "Ksh" + " " + 70000,
